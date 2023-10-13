32ec9d095237cb1d23362ed2dcf585d9fe3036c4
kohait00 <konstantin.hartwich@gmail.com>
2023-10-13 10:14:05 +0200

    remove component.mk, unneeded with current IDF releases

--- component.mk
+++
@@ -1,4 +0,0 @@
-ESP_IDF=${IDF_PATH} 
-COMPONENT_ADD_INCLUDEDIRS := include ${ESP_IDF}/tools/unit-test-app/components/unity/include/ library/ccronexpr library/jobs .
-COMPONENT_SRCDIRS :=  library/ccronexpr library/jobs test src .
-CFLAGS += -D CRON_USE_LOCAL_TIME
